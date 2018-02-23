6c3393b1f49b4aff4083760fd5630d0b82f5bbce
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2018-02-23 13:47:50 +0100

    Update init.R

--- init.R
+++ init.R
@@ -5,3 +5,7 @@ devtools::install_github('keboola/r-application', ref = "master", force = TRUE)
 devtools::install_github('keboola/r-docker-application', ref = "master", force = TRUE)
 devtools::install_github("keboola/sapi-r-client", ref = "master", force = TRUE)
 devtools::install_github("cloudyr/aws.signature")
+
+# install really required packages
+withCallingHandlers(install.packages(
+    c('ff')), warning = function(w) stop(w))
