5453fcaab75901d8d85e24019cdd1fffd8694635
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2018-05-01 14:42:07 +0200

    Update main.R

--- main.R
+++ main.R
@@ -172,6 +172,9 @@ write_endpoint<-function(endpoint,token,from=NULL,limit=1000){
            app$writeTableManifest(paste0("/data/out/tables/",endpoint[[3]],".csv"),destination='', primaryKey=c('item_name
 '),incremental=TRUE)
     	}
+     else if (endpoint[[3]]=="") {
+      app$writeTableManifest("/data/out/tables/out_log.csv",destination='', primaryKey=c('date','endpoint'), incremental=TRUE)
+      }
      else
           app$writeTableManifest(paste0("/data/out/tables/",endpoint[[3]],".csv"),destination='', primaryKey='',incremental= FALSE)
          }
