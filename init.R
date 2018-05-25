ffef4962749ad5033882eeee869b95fa72f11c92
RevoltBI <giuliano@revolt.bi>
2018-05-25 16:59:50 +0200

    Cloudyr Update Dockerfile

--- init.R
+++ init.R
@@ -4,8 +4,9 @@ library('devtools')
 devtools::install_github('keboola/r-application', ref = "master", force = TRUE)
 devtools::install_github('keboola/r-docker-application', ref = "master", force = TRUE)
 devtools::install_github("keboola/sapi-r-client", ref = "master", force = TRUE)
-devtools::install_github("cloudyr/aws.signature")
+install.packages("aws.ec2metadata", repos = c(cloudyr = "http://cloudyr.github.io/drat", getOption("repos")))
+install.packages("aws.signature", repos = c(cloudyr = "http://cloudyr.github.io/drat", getOption("repos")))
 
 # install really required packages
-withCallingHandlers(install.packages("future" , force = TRUE), warning = function(w) stop(w))
-withCallingHandlers(devtools::install_github("DavisVaughan/furrr", force = TRUE),warning = function(w) stop(w))
+withCallingHandlers(install.packages("future"), warning = function(w) stop(w))
+withCallingHandlers(devtools::install_github("DavisVaughan/furrr"),warning = function(w) stop(w))
