326331b91bc5cf1b377a70519b876e3afe27ba3e
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2018-05-27 14:22:12 +0200

    Update init.R

--- init.R
+++ init.R
@@ -3,9 +3,9 @@ library('devtools')
 # install the transformation application ancestors
 devtools::install_github('keboola/r-application', ref = "master", force = TRUE)
 devtools::install_github('keboola/r-docker-application', ref = "master", force = TRUE)
-devtools::install_github("keboola/sapi-r-client", ref = "master", force = TRUE)
-devtools::install_github("cloudyr/aws.signature")
-devtools::install_github("DavisVaughan/furrr", ref = "master", force = TRUE) 
+devtools::install_github('keboola/sapi-r-client', ref = "master", force = TRUE)
+devtools::install_github('cloudyr/aws.signature')
+devtools::install_github('DavisVaughan/furrr', ref = "master", force = TRUE) 
 
 # install really required packages
 withCallingHandlers(install.packages(
