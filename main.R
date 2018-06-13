c5086017f1a529818f5e2afa38fbfd0023a287d0
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2018-06-13 13:26:51 +0200

    Update main.R

--- main.R
+++ main.R
@@ -208,7 +208,7 @@ write_endpoint<-function(endpoint,token,from=NULL,limit=1000){
             .$result%>%.$data%>%as_data_frame%>%sanitize(endpoint[[4]],endpoint[[3]])
           
           #If i = 0 then initialize the file else append the csv using fwrite from data.table in order to not waste RAM
-          fwrite(res,paste0("./data/out/tables/",prefix,endpoint[[3]],".csv"),append = ifelse(i>0,TRUE,FALSE), sep=",", sep2=c("{","|","}"))
+          fwrite(res,paste0("/data/out/tables/",prefix,endpoint[[3]],".csv"),append = ifelse(i>0,TRUE,FALSE), sep=",", sep2=c("{","|","}"))
           
           #csvFileName<-paste0("/data/out/tables/",prefix,endpoint[[3]],".csv")
           #write.csv(res,file=csvFileName,row.names = FALSE)
@@ -226,10 +226,10 @@ write_endpoint<-function(endpoint,token,from=NULL,limit=1000){
   
   #Process log info
   ## Check if out_log.csv exists
-  logfile_created<-file.exists(paste0("./data/out/tables/",prefix,"log.csv"))
+  logfile_created<-file.exists(paste0("/data/out/tables/",prefix,"log.csv"))
   
   log<-data_frame("date"=Sys.time(),"endpoint"=endpoint[[3]],"exported_records"=total,"extraction_time"=time, "call"=call)
-  fwrite(log,paste0("./data/out/tables/",prefix,"log.csv"),append=logfile_created)
+  fwrite(log,paste0("/data/out/tables/",prefix,"log.csv"),append=logfile_created)
   
   
   #Writes the manifest file
@@ -237,10 +237,10 @@ write_endpoint<-function(endpoint,token,from=NULL,limit=1000){
   ## define secondary key by default due to "activities" addition in column name 
   if(endpoint[[3]] %in% c("activitiesChat","activitiesEmail","activitiesCall")){endpoint[[4]]["secondary_key"]<-"activities_name"}
   
-  if(logfile_created) app$writeTableManifest(paste0("./data/out/tables/",prefix,"log.csv"), primaryKey = c("date","endpoint"),incremental = incr_load,destination="in.c-DaktelaTest")
+  if(logfile_created) app$writeTableManifest(paste0("/data/out/tables/",prefix,"log.csv"), primaryKey = c("date","endpoint"),incremental = incr_load,destination="in.c-DaktelaTest")
   
   
-  app$writeTableManifest(paste0("./data/out/tables/",prefix,endpoint[[3]],".csv"), primaryKey = endpoint[[4]][names(endpoint[[4]]) %in% c("primary_key","secondary_key")]%>%str_replace_all("\\.","_"),incremental = incr_load, destination="in.c-DaktelaTest")
+  app$writeTableManifest(paste0("/data/out/tables/",prefix,endpoint[[3]],".csv"), primaryKey = endpoint[[4]][names(endpoint[[4]]) %in% c("primary_key","secondary_key")]%>%str_replace_all("\\.","_"),incremental = incr_load, destination="in.c-DaktelaTest")
   
 }
 
