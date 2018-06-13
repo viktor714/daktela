1d69e58a0816d74f04db8db95a224f6a093a7237
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2018-06-13 21:41:00 +0200

    Update init.R

--- init.R
+++ init.R
@@ -1,6 +1,6 @@
 # install really required packages
 withCallingHandlers(install.packages(
-    c('devtools','future','dplyr','data.table','readr','tidyr','digest','stringr','lubridate','purrr','jsonlite','httr'), 
+    c('devtools','future','dplyr','data.table','readr','tidyr','digest','stringr','lubridate','purrr','httr'), 
     lib = "/usr/local/lib/R/site-library/"), warning = function(w) stop(w))
 
 # install the R application
@@ -8,3 +8,4 @@ devtools::install_github('keboola/r-docker-application', ref = "2.0.2")
 devtools::install_github('keboola/r-application', ref = "master", force = TRUE)
 devtools::install_github('DavisVaughan/furrr', ref = "master", force = TRUE) 
 devtools::install_github('hadley/dplyr', ref = "master", force = TRUE)
+devtools::install_github('jeroen/jsonlite', ref = "master", force = TRUE)
