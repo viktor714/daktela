8f213aa00c97b8eca41832ac26785d60669f358b
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2018-06-30 13:58:20 +0200

    Update main.R

--- main.R
+++ main.R
@@ -51,6 +51,13 @@ increment<-app$getParameters()$incr
 ## Destination bucket selection
 #bucket<-app$getParameters()$destination
 
+
+##Catch config errors
+
+if(is.null(user)) stop("enter your username in the user config field")
+if(is.null(pwd)) stop("enter your password in the #password config field")
+if(is.null(server)) stop("enter API server")
+
 tryCatch(
     { sink(NULL)},
     warning = function(w) {})	
