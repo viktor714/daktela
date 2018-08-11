ec0a1c209fba1c8ac2ad7e4c2d6e77626bfb152d
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2018-08-11 08:50:05 +0200

    Update main.R

--- main.R
+++ main.R
@@ -230,18 +230,18 @@ write_endpoint<-function(endpoint,token,from=NULL,limit=1000){
             fromJSON(flatten = TRUE, simplifyDataFrame = TRUE) %>%
             .$result%>%.$data%>%as_data_frame%>%sanitize(endpoint[[4]],endpoint[[3]])
             
-           if (endpoint[[3]]=="activities")  {
-                     res2<-lapply(res, flatten)
-                   res<-do.call(data.frame,res2)
+           # v1.9.3 addition  if (endpoint[[3]]=="activities")  {
+             # v1.9.3 addition         res2<-lapply(res, flatten)
+               # v1.9.3 addition    res<-do.call(data.frame,res2)
                    
                    #replace NULLs by NAs
-                  res$statuses.V8[res$statuses.V8=="NULL"]<- NA
+                # v1.9.3 addition  res$statuses.V8[res$statuses.V8=="NULL"]<- NA
                    
                    #add status.name to the appropriate column
-                     res$status.name<-paste(res$status_name,res$statuses.V8)
+                  # v1.9.3 addition   res$status.name<-paste(res$status_name,res$statuses.V8)
                     #drop columns with statuses info
-                 } 
-                  else {res}    
+                # v1.9.3 addition } 
+                  # v1.9.3 addition  else {res}    
           
           #If i = 0 then initialize the file else append the csv using fwrite from data.table in order to not waste RAM
           fwrite(res,paste0("/data/out/tables/",prefix,endpoint[[3]],".csv"),append = ifelse(i>0,TRUE,FALSE), sep=",", sep2=c("{","|","}"))
@@ -547,7 +547,7 @@ names_activities <-
     "time_close",
     "important",
     "status"
-    ,"statuses"
+    # v1.9.3 addition ,"statuses"
   )
 
 
