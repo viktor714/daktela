a3f9301f7bc03bc86deacef784bede2899a79872
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2018-08-13 19:23:29 +0200

    Update main.R

--- main.R
+++ main.R
@@ -239,7 +239,8 @@ write_endpoint<-function(endpoint,token,from=NULL,limit=1000){
                 res[res == "NULL"] = NA
                 res$statuses.V8<-unlist(res$statuses.V8) 
                 res<-res[, sapply(res, class) != "list"] 
-                names(res)<-str_replace_all(names(res),"\\.","_")
+                names(res$statuses.V8)<-"statuses_V8"
+              
                    
                    #add status.name to the appropriate column
                    #res$status_name<-paste(res$status_name,res$statuses.V8)
